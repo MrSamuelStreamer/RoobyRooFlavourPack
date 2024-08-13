0466503d28f349f7f78ac48282ddda611635129d
Dexter Lowe <feldoh10@gmail.com>
2024-08-13 21:14:28 +0100

    Upgrade layout

---
+++ 1.5/Source/Rooby_Roo_Flavour_Pack/Rooby_Roo_Flavour_PackDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rooby_Roo_Flavour_Pack;
+
+[DefOf]
+public static class Rooby_Roo_Flavour_PackDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static Rooby_Roo_Flavour_PackDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(Rooby_Roo_Flavour_PackDefOf));
+}
---
+++ 1.5/Source/Rooby_Roo_Flavour_Pack/Rooby_Roo_Flavour_PackMod.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Rooby_Roo_Flavour_Pack;
+
+public class Rooby_Roo_Flavour_PackMod : Mod
+{
+    public static Settings settings;
+
+    public Rooby_Roo_Flavour_PackMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("Taggerung.rimworld.Rooby_Roo_Flavour_Pack.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Rooby Roo Flavour Pack_SettingsCategory".Translate();
+    }
+}
